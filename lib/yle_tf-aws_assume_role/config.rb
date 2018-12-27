55b4592a08cc8e035d191bfce6905606a75a2264
Juha Ylitalo <juha@ylitalot.net>
2018-12-27 13:25:58 +0200

    Drops support for Ruby v2.2, update rest and add support for v2.6 (#4)
    Drops support for Ruby v2.2, update rest and add support for v2.6

--- lib/yle_tf-aws_assume_role/config.rb
+++ lib/yle_tf-aws_assume_role/config.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module YleTfPlugins
   module AWSAssumeRole
     class Config
--- spec/yle_tf-aws_assume_role/plugin_spec.rb
+++ spec/yle_tf-aws_assume_role/plugin_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 require 'yle_tf-aws_assume_role/plugin'
